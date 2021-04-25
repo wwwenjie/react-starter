532fba460548ebc9a20471dabcb3e33c63eac45c
wenjie <jinwenjie@live.com>
2021-04-25 19:05:04 +0800

    chore: add stylelint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line functional/immutable-data
 module.exports = {
-  '**/*.{ts,tsx,md,html,json}': 'prettier --check',
+  '**/*.{ts,tsx,md,css,html,json}': 'prettier --check',
   'src/**/*.{ts,tsx}': 'eslint',
   '{README.md,.github/*.md,src/**/**}': 'cspell',
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',

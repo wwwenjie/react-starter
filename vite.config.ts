d135b8320693dae0cde0ca139b71e41de1d10c4c
wenjie <jinwenjie@live.com>
2022-02-13 21:07:03 +0800

    [wenjie] #N/A chore: add packge visualizer, add plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths(), svgr()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    visualizer({ filename: 'node_modules/.visualizer/stats.html' }),
+  ],
 })

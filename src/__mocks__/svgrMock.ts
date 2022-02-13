05cbdd7f10c3bc7bef87375b305b58a536dac029
wenjie <jinwenjie@live.com>
2022-02-13 22:46:39 +0800

    [wenjie] #N/A chore: remove useless mocks

--- src/__mocks__/svgrMock.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-// https://github.com/gregberge/svgr/issues/83#issuecomment-626307111
-import React from 'react'
-
-export default 'SvgrURL'
-export const ReactComponent = 'div'

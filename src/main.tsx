03ebbb7d81f2d4c049debe1d310c88dc063f54aa
wenjie <wenjie.jin@thoughtworks.com>
2025-07-17 22:16:46 +0800

    chore: add antd support for react 19

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import '@ant-design/v5-patch-for-react-19'
 import '@/styles/tailwind.css'
 import '@/styles/main.css'
 
